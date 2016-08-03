edc8f0d4aed9331b749ff65f149be95c93eefb21
Joe Ruello <joeruello@gmail.com>
2016-08-03 14:10:20 +1000

    app

--- src/App.js
+++ src/App.js
@@ -6,16 +6,37 @@ import 'babel-polyfill';
 
 
 class App extends Component {
+
+    tick(){
+        const now = moment().format('X');
+        this.setState({now});
+        this.setState({timeout: setTimeout(this.tick, 100)});
+    }
+
+    constructor(){
+        super();
+        this.tick = this.tick.bind(this);
+    }
+
+    componentDidMount(){
+        this.tick();
+    };
+
+
+    state = {
+      now: null
+    }
+
   render() {
 
     const hackStart = moment('2016-08-03 16:30').format('X');
-    const hackEnd = moment('2016-08-05 13:30').format('X');
-    const now = moment();
+    const hackEnd = moment('2016-08-05 15:30').format('X');
+    const now = moment().format('X');
     const hasStarted = hackStart < now;
 
     return (
-      <div className="center">
-      <div className="container">
+      <div className={`center ${hasStarted && 'dark'}`}>
+      <div className={`container`}>
         <div className="row">
           <div className="col-xs-3"/>
           <div className="col-xs-6 logo"/>
@@ -24,10 +45,10 @@ class App extends Component {
           <div className="col-xs-12">Hackathon 2016</div> 
         </div>
         <div className="row">
-        <Clock timestamp={hasStarted ? hackStart : hackEnd}/>        
+        <Clock timestamp={hasStarted ? hackEnd : hackStart}/>        
         </div>
         <div className="row text-center big">
-          {hasStarted ? 'Pitches and Kickoff' : 'Hack ends'}
+          {hasStarted ? 'Hack ends' : 'Pitches and Kickoff'}
         </div>
       </div>
       </div>
