e1b741c7e864515a7aaa7da1f86e32710558a1ac
Joe Ruello <joeruello@gmail.com>
2017-08-09 11:28:12 +1000

    Things

--- src/App.js
+++ src/App.js
@@ -30,27 +30,27 @@ class App extends Component {
   render() {
 
     const hackStart = moment('2017-08-09 15:30').format('X');
-    const hackEnd = moment('2017-08-11 15:30').format('X');
+    const hackEnd = moment('2017-08-11 15:00').format('X');
     const now = moment().format('X');
     const hasStarted = hackStart < now;
-
+    console.log("WOW");
     return (
       <div className={`center ${hasStarted && 'dark'}`}>
-      <div className={`container`}>
-        <div className="row">
-          <div className="col-xs-3"/>
-          <div className="col-xs-6 logo"/>
+        <div className={`container`}>
+          <div className="row">
+            <div className="col-xs-3"/>
+            <div className="col-xs-6 logo"/>
+          </div>
+          <div className="row text-center big">
+            <div className="col-xs-12">Hackathon 2017</div> 
+          </div>
+          <div className="row">
+          <Clock timestamp={hasStarted ? hackEnd : hackStart}/>        
+          </div>
+          <div className="row text-center big">
+            {hasStarted ? 'Hack ends' : 'Kickoff'}
+          </div>
         </div>
-        <div className="row text-center big">
-          <div className="col-xs-12">Hackathon 2017</div> 
-        </div>
-        <div className="row">
-        <Clock timestamp={hasStarted ? hackEnd : hackStart}/>        
-        </div>
-        <div className="row text-center big">
-          {hasStarted ? 'Hack ends' : 'Kickoff'}
-        </div>
-      </div>
       </div>
     );
   }
